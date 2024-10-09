9acd5a21bd1b18650f46acee64fe5949e499db46
Byrone Kinsly <Kinslybyrone17@gmail.com>
2024-10-09 11:15:29 +0300

    created a skeleton for singleBlog and LatestNews to render before data is fetched. Added default image incase author has no image

--- frontend/src/Pages/Blog.jsx
+++ frontend/src/Pages/Blog.jsx
@@ -7,11 +7,14 @@ import { useParams } from 'react-router-dom';
 import Footer from '../Component/Footer';
 import Comments from '../Component/Comments';
 import ViewComment from '../Component/ViewComment';
+import SingleBlogSkeleton from '../Skeleton/SingleBlogSkeleton';
 
 function Blog() {
   const apiBaseUrl = import.meta.env.VITE_API_BASE_URL
-  const {category, slug } = useParams(); // Destructure id from useParams
+  const {category, slug } = useParams(); 
   const [postData, setPostData] = useState({});
+  const [loading, setLoading] = useState(true);
+
 
   const formatToLocalTime = (dateString) => {
     const options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', hour12: false,  };
@@ -20,46 +23,67 @@ function Blog() {
 
   useEffect(() => {
     const apiUrl = `${apiBaseUrl}/api/v1/user/${category}/${slug}`;
+    setLoading((prevState) => {
+      !prevState;
+    });
 
     fetch(apiUrl)
       .then((response) => response.json())
       .then((data) => {
-        console.log(`This is the data: ${data}`)
         if (typeof data === 'object' && !Array.isArray(data)) {
           setPostData(data);
+         
         } else {
           console.error('API response is not an object:', data);
         }
       })
       .catch((error) => console.error(`Error fetching ${category} data:`, error));
-  }, []);
+      setLoading((prevState) => {
+        !prevState;
+      });
+  }, [apiBaseUrl, category, slug]);
 
 
   return (
     <div>
       <Navbar />
       <div className=" py-24 flex flex-col md:flex-row   ">
-        <div className="md:px-5 flex-2 bg-[#f5f5f5] max-w-screen p-8  m-2    px-4">
+        {Object.keys(postData).length  == 0  ? (
+          <SingleBlogSkeleton/>
+        ) :(
+          <div className="md:px-5 flex-2 bg-[#f5f5f5] max-w-screen p-8  m-2    px-4">
           <div className=" justify-even">
-            {/* Check if postData has data before rendering */}
-            {Object.keys(postData).length > 0 && (
-              <Content
-                className=""
-                key={slug}
-                id={postData.id}
-                title={postData.title}
-                slug={postData.slug}
-                content={postData.content}
-                author={postData.author}
-                author_image={postData.author_image}
-                published_on={formatToLocalTime(postData.published_on)}
-                image={postData.image}
-              />
-            )}
+
+         
+              
+              {postData.length != 0 && (
+                <Content
+                  className=""
+                  key={slug}
+                  id={postData.id}
+                  title={postData.title}
+                  slug={slug}
+                  content={postData.content}
+                  author={postData.author}
+                  author_image={postData.author_image}
+                  published_on={formatToLocalTime(postData.published_on)}
+                  image={postData.image}
+                />
+              )}
+           
+
+           
           </div>
         </div>
+        )}
+       
         <div className="bg-[#f5f5f5] md:block flex-1">
+          {loading ?(
+            <SingleBlogSkeleton/>
+          ):(
+
           <SidePanel />
+          )}
         </div>
       </div>
 
